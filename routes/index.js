8f81edfcd4eb9c06c2555e90cb9e68dbdc8ba978
Bhargavi Addagulla <bhargavi@crowdspots.com>
2018-11-23 18:47:12 +0530

    changed the code according to the new schema and conventions

--- routes/index.js
+++ routes/index.js
@@ -1,261 +1,210 @@
-// var express = require('express');
-// var router = express.Router();
-// // var path = require('path');
-// var fetchTags = require("./fetchTopTag.js");
-// /* GET home page. */
-
-// exports.tag=function(req,res){
-//   var abc =[];
-//   var abc=fetchTags.getTags();
-    
-//      res.render('todo',{todos:abc});
-
-// };
-// exports.index = function(req, res){
-//   res.render('index', { title: 'TODO APP' });
-// };
 var async = require("async");
-/* GET home page. */
 var elasticsearch = require('elasticsearch');
+
 var client = new elasticsearch.Client({});
 client.ping({
-     requestTimeout: 30000,
- }, function(error) {
-     if (error) {
-         console.error('elasticsearch cluster is down!');
-     } else {
-         console.log('Everything is ok');
-     }
- });
-var fetchTags = require("./fetchTopTag.js");
-
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Youtube Transcript & Keywords' });
-};
-exports.about= function(req,res){
-  res.render('index',{title:'about'});
-};
-exports.mainPage=function(req,res){
-	console.log(req.url);
-  var result = [];
-client.search({  
-  index: 'caption_you',
-  type: 'wordCount',
-  body: {
-        sort: [{ "count": { "order": "desc" } }],
-        size: 10,
-        query: { match_all: {}}
-     }
-},function (error, response,status) {
-    if (error){
-      console.log("search error: "+error)
-    }
-    else {
-      // console.log("--- Response ---");
-      // console.log(response);
-      // console.log("--- Hits ---");
-      response.hits.hits.forEach(function(hit){
-      // console.log(hit._source.words,hit._source.count);
-      result.push(hit._source.words);
-        // result.push(hit._source.words);
-      })
-      // console.log(result);
-     res.render('mainPage',{tags:result}); 
-      
-      // return result;
-    }
-
+	requestTimeout: 30000,
+}, function(error) {
+	if (error) {
+		console.error('elasticsearch cluster is down!');
+	} else {
+		console.log('Everything is ok');
+	}
 });
 
-// console.log(abc);
-  // console.log(fetchTags.getTags());
-     // console.log("abcd123",abc);
-
-
+exports.index = function(req, res){
+	res.render('index', { title: 'Youtube Transcript & Keywords' });
 };
 
+exports.about= function(req,res){
+	res.render('index',{title:'about'});
+};
+
+exports.mainPage=function(req,res){
+	var result = [];
+	client.search({  
+		index: 'youtube_entities',
+		type: 'entities',
+		body: {
+			sort: [{ "frequency": { "order": "desc" } }],
+			size: 30,
+			query: { match_all: {}}
+		}
+	},function (error, response,status) {
+		if (error){
+			console.log("search error: "+error)
+		}
+		else {
+			response.hits.hits.forEach(function(hit){
+				if(result.indexOf(hit._source.text) == -1){
+					result.push(hit._source.text);
+				}
+			});
+
+			result = result.slice(0,10);
+			console.log(result);
+		 	res.render('mainPage',{tags:result}); 
+		}
+	});
+};
 
 exports.searchVideoTag= function(req,res){
-  var url=req.url;
-  console.log("abc",url);
-  var index = url.lastIndexOf('/');
-  var queryTerm = url.slice(index+1);
-  var result = [];
-  var videoIds = [];
-  // console.log(tag);
-  client.search({
-      index: 'caption_you',
-      type: 'wordCount',
-        body: {
-          sort: [{ "count": { "order": "desc" } }],
-          size: 10,
-          query: {
-              match: {
-                "words": queryTerm
-            }
-        }
-    }
-    },function (error, response,status) {
-    if (error){
-      console.log("search error: "+error)
-    }
-    else {
-      response.hits.hits.forEach(function(hit){
-       var count=videoIds.length;
-       var flag=false;
-      for(var i=0;i<=count;i++)
-      {
-        if(videoIds[i]==hit._source.videoId){
-          flag=true;
-        }
-      }
-      if(!flag)
-        // result.push(populateVideo(hit._source.videoId);
-        videoIds.push(hit._source.videoId);
-      })
-      // console.log("videoIds",videoIds);
-      // var videoIdCount = 9999999;
-      var i=1;
-      videoIds.forEach(function(videoId){
-    client.search({
-      index: 'caption_you',
-      type: 'trans',
-      // sort: [{ "count": { "order": "desc" } }],
-    //       size: 10,
-        body: {
-          size: 10,
-          query: {
-              match: {
-                "_id":videoId
-            }
-        }
-        }
-    },function (error, response,status) {
-    if (error){
-      console.log("search error: "+error)
-       }
-       else{  
-        var videoIdCount = videoIds.length;
-          // console.log(response.hits.hits);
-        var temp = {};
-        temp["videoId"]=response.hits.hits[0]._source.videoId;
-        temp["title"]=response.hits.hits[0]._source.title;
-        temp["description"]=response.hits.hits[0]._source.description;
-        temp["transcript"]=response.hits.hits[0]._source.transcript;
+	var url=req.url;
+	console.log("abc",url);
+	var index = url.lastIndexOf('/');
+	var queryTerm = url.slice(index+1);
+	var result = [];
+	var videoIds = [];
+	client.search({
+		index: 'youtube_entities',
+		type: 'entities',
+			body: {
+				sort: [{ "frequency": { "order": "desc" } }],
+				size: 10,
+				query: {
+					match: {
+						"text": queryTerm
+					}
+				}	
+			}
+	},function (error, response,status) {
+		if (error){
+			console.log("search error: "+error)
+		}
+		else {
+			response.hits.hits.forEach(function(hit){
+				var count = videoIds.length;
+				var flag = false;
+				for(var i = 0;i <= count;i++)
+				{
+					if(videoIds[i] == hit._source.videoId){
+						flag = true;
+					}
+				}
+				if(!flag)
+					videoIds.push(hit._source.videoId);
+			});
 
-        // console.log("temp",temp);
-        result.push(temp);
-        if(i==videoIdCount)
-        {
-           res.render('search',{links:result}); 
-        }
-        else{
-          i++
-        }
-
-       }      
-      })
-  })
-        }
-
-  });
-  // res.render('search', { title:  tag });
+			var i = 1;
+			videoIds.forEach(function(videoId){
+				client.search({
+					index: 'youtube_entities',
+					type: 'youtube_meta',
+					body: {
+						size: 10,
+						query: {
+							match: {
+								"_id":videoId
+							}
+						}
+					}
+				},function (error, response,status) {
+					if (error){
+						console.log("search error: "+error)
+			 		}
+			 		else{  
+						var videoIdCount = videoIds.length;
+						var temp = {};
+						temp["videoId"]=response.hits.hits[0]._source.videoId;
+						temp["title"]=response.hits.hits[0]._source.title;
+						temp["description"]=response.hits.hits[0]._source.description;
+						temp["transcript"]=response.hits.hits[0]._source.transcript;
+						result.push(temp);
+						if(i == videoIdCount){
+						 	res.render('search',{links:result}); 
+						}
+						else{
+							i++
+						}
+					}      
+				})
+			})
+		}
+	});
 };
+
 exports.showTranscript=function(){
-  console.log($('.viewMore'));
+	console.log($('.viewMore'));
 }
 
-
 exports.searchVideo=function(req,res){ 
-		var url=req.url;
-		console.log("abc",url);
-		// console.log(req);
-		var result = [];
-		var videoIds = [];
-		var queryTerm={};
-		console.log(url.lastIndexOf("/search?q="));
-		if(url.lastIndexOf("/search?q=")!=-1)
-		{	console.log("true");
-			queryTerm=url.slice(url.lastIndexOf("/search?q"));
+	var url=req.url;
+	var result = [];
+	var videoIds = [];
+	var queryTerm={};
+	if(url.lastIndexOf("/search?q=")!=-1){
+		queryTerm=url.slice(url.lastIndexOf("/search?q"));
+	}
+	if(url=="/search"){
+		queryTerm=req.body['search_query'];
+	}
+
+	client.search({
+		index: 'youtube_entities',
+		type: 'entities',
+		body: {
+			sort: [{ "frequency": { "order": "desc" } }],
+			size: 10,
+			query: {
+				match: {
+					"text": queryTerm
+				}
+			}
 		}
-		if(url=="/search")
-        queryTerm=req.body['search_query'];
-    	console.log(queryTerm);
- 		client.search({
-		  index: 'caption_you',
-		  type: 'wordCount',
-	      body: {
-	      	sort: [{ "count": { "order": "desc" } }],
-        	size: 10,
-        	query: {
-            	match: {
-                "words": queryTerm
-            }
-        }
-    }
-		},function (error, response,status) {
-    if (error){
-      console.log("search error: "+error)
-    }
-    else {
-      response.hits.hits.forEach(function(hit){
-       var count=videoIds.length;
-       var flag=false;
-    	for(var i=0;i<=count;i++)
-    	{
-        if(videoIds[i]==hit._source.videoId){
-        	flag=true;
-        }
-    	}
-    	if(!flag)
-    		// result.push(populateVideo(hit._source.videoId);
-    		videoIds.push(hit._source.videoId);
-      })
-      // console.log("videoIds",videoIds);
-      // var videoIdCount = 9999999;
-      var i=1;
-      videoIds.forEach(function(videoId){
-		client.search({
-		  index: 'caption_you',
-		  type: 'trans',
-		  // sort: [{ "count": { "order": "desc" } }],
-    //       size: 10,
-	      body: {
-        	size: 10,
-        	query: {
-            	match: {
-                "_id":videoId
-            }
-        }
-    		}
-		},function (error, response,status) {
-    if (error){
-      console.log("search error: "+error)
-   		 }
-   		 else{	
-   		 	var videoIdCount = videoIds.length;
-   		 		console.log(response.hits.hits[0]._source);
-   		 	var temp = {};
-    		temp["videoId"]=response.hits.hits[0]._source.videoId;
-    		temp["title"]=response.hits.hits[0]._source.title;
-    		temp["description"]=response.hits.hits[0]._source.description;
-        temp["thumbnail"]=response.hits.hits[0]._source.thumbnails;
-        temp["transcript"]=response.hits.hits[0]._source.transcript;
-    		// console.log("temp",temp);
-    		result.push(temp);
-    		if(i==videoIdCount)
-    		{
-    			 res.render('search',{links:result}); 
-    		}
-    		else{
-    			i++
-    		}
+	},function (error, response,status) {
+		if (error){
+			console.log("search error: "+error)
+		}
+		else {
+			response.hits.hits.forEach(function(hit){
+				var count = videoIds.length;
+			 	var flag=false;
+				for(var i=0;i<=count;i++)
+				{
+					if(videoIds[i]==hit._source.videoId){
+						flag=true;
+					}
+				}
+				if(!flag){
+					videoIds.push(hit._source.videoId);
+				}
+			})
 
-   		 }      
-   		})
-	})
-	    	}
-
+			var i = 1;
+			videoIds.forEach(function(videoId){
+				client.search({
+					index: 'youtube_entities',
+					type: 'youtube_meta',
+					body: {
+						size: 10,
+						query: {
+							match: {
+								"_id":videoId
+							}
+						}
+					}
+				},function (error, response,status) {
+					if (error){
+						console.log("search error: "+error)
+			 		}
+			 		else{	
+						var videoIdCount = videoIds.length;
+						var temp = {};
+						temp["videoId"]=response.hits.hits[0]._source.videoId;
+						temp["title"]=response.hits.hits[0]._source.title;
+						temp["description"]=response.hits.hits[0]._source.description;
+						temp["thumbnail"]=response.hits.hits[0]._source.thumbnail;
+						temp["transcript"]=response.hits.hits[0]._source.transcript;
+						result.push(temp);
+						if(i == videoIdCount){
+							res.render('search',{links:result}); 
+						}
+						else{
+							i++
+						}
+					}      
+				})
+			})
+		}
 	});
 };
