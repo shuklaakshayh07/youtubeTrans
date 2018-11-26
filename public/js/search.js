6a0e17afb9254747cc389a3de29a245316829a73
Bhargavi Addagulla <bhargavi@crowdspots.com>
2018-11-26 17:24:17 +0530

    removed log statements and UI changes

--- public/js/search.js
+++ public/js/search.js
@@ -1,58 +1,50 @@
-
 window.showTranscript = showTranscript;
 
-var showTranscript=function(id){
-	var transcriptDiv="viewMore-"+id;
- 	// console.log($("."+transcriptDiv).show());
- 	// console.log($("."+transcriptDiv).css("display","block"));
- 	if($("#"+transcriptDiv).hasClass('hide'))
- 	$("#"+transcriptDiv).removeClass('hide');
- 	else
- 	$("#"+transcriptDiv).addClass('hide');	
+var showTranscript = function(id){
+	var transcriptDiv = "viewMore-"+id;
+ 	if($("#"+transcriptDiv).hasClass('hide')){
+ 		$("#"+transcriptDiv).removeClass('hide');
+ 	}
+ 	else{
+ 		$("#"+transcriptDiv).addClass('hide');	
+ 	}
 }
+
 window.highlightWord = highlightWord;
-var highlightWord=function(id){
-	var searchInputId="search-"+id;
-	var keywordQuery=$("#"+searchInputId).val().toUpperCase();
-	var transcript=""+String($("#transcript-"+id).html());
-	transcript=resetFormatting(transcript);
-	console.log("transcript",transcript);
-	var tempTranscript=transcript.toUpperCase();
-	var index=tempTranscript.indexOf(keywordQuery);
 
-	console.log("index",index);
-	var queryLen=keywordQuery.length;
-	var result="";
-	while(index!=-1)
-			{	
-				result+=transcript.substring(0,index);
-				transcript=transcript.substring(index);
-				result+="<mark>"+transcript.substring(0,queryLen)+"</mark> ";
-				transcript=transcript.substring(queryLen);
-				tempTranscript=tempTranscript.substring(index+queryLen);
-				index=tempTranscript.indexOf(keywordQuery);
-				// console.log(result,":::::::");
-			}
-	result+=transcript;
+var highlightWord = function(id){
+	var searchInputId = "search-"+id;
+	var keywordQuery = $("#"+searchInputId).val().toUpperCase();
+	var transcript = ""+String($("#transcript-"+id).html());
+	transcript = resetFormatting(transcript);
+	var tempTranscript = transcript.toUpperCase();
+	var index = tempTranscript.indexOf(keywordQuery);
 
-
-	console.log(result);
+	var queryLen = keywordQuery.length;
+	var result = "";
+	while(index != -1){	
+		result += transcript.substring(0,index);
+		transcript = transcript.substring(index);
+		result += "<mark>"+transcript.substring(0,queryLen)+"</mark> ";
+		transcript = transcript.substring(queryLen);
+		tempTranscript = tempTranscript.substring(index+queryLen);
+		index = tempTranscript.indexOf(keywordQuery);
+	}
+	result += transcript;
 	$("#transcript-"+id).find("p").replaceWith(result);
 }
-var resetFormatting=function(temp){
-	var transcript="";
-	var index=temp.indexOf("<mark>");
-	while(index!=-1)
-		{
-			transcript+=temp.substring(0,index);
-			temp=temp.substring(index+6);
-			var secIndex=temp.indexOf("</mark>");
-			transcript+=temp.substring(0,secIndex);
-			temp=temp.substring(secIndex+7);
-			index=temp.indexOf("<mark>");
-		}
-		transcript+=temp;
-		console.log(transcript);
+
+var resetFormatting = function(temp){
+	var transcript = "";
+	var index = temp.indexOf("<mark>");
+	while(index != -1){
+		transcript += temp.substring(0,index);
+		temp = temp.substring(index+6);
+		var secIndex = temp.indexOf("</mark>");
+		transcript += temp.substring(0,secIndex);
+		temp = temp.substring(secIndex+7);
+		index = temp.indexOf("<mark>");
+	}
+	transcript += temp;
 	return transcript;
 }
-// exports.stopVidAndCloseModal = stopVidAndCloseModal;
